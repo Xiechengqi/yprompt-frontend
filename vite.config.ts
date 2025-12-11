fbcf8dd4e8b741b293988ca241094643be04ef51
root <root@ai.dev>
2025-12-11 12:03:44 +0800

    add

--- vite.config.ts
+++ vite.config.ts
@@ -39,7 +39,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8888',
+        target: 'http://localhost:8002',
         changeOrigin: true,
         secure: false
       }
